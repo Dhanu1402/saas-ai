5b9fb6ddc84e52aa9e2d866be3d34db6ebfd1293
dhanu1402 <dhanukumar2000@gmail.com>
2024-02-16 20:54:10 +0530

    video generation (replicate ai)

--- app/(dashboard)/(routes)/video/page.tsx
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import * as z from 'zod';
-import { Music } from 'lucide-react';
+import { VideoIcon } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
@@ -17,10 +17,10 @@ import { Loader } from '@/components/loader';
 
 import { formSchema } from './constants';
 
-const MusicPage = () => {
+const VideoPage = () => {
   const router = useRouter();
 
-  const [music, setMusic] = useState<string>();
+  const [video, setVideo] = useState<string>();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -34,11 +34,11 @@ const MusicPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      setMusic(undefined);
+      setVideo(undefined);
 
-      const response = await axios.post('/api/music', values);
+      const response = await axios.post('/api/video', values);
 
-      setMusic(response.data.audio);
+      setVideo(response.data[0]);
 
       form.reset();
     } catch (error: any) {
@@ -52,11 +52,11 @@ const MusicPage = () => {
   return (
     <div>
       <Heading
-        title="Music Generation"
-        description="Turn your prompt into music."
-        icon={Music}
-        iconColor="text-emerald-500"
-        bgColor="bg-emerald-500/10"
+        title="Video Generation"
+        description="Turn your prompt into video."
+        icon={VideoIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
       />
 
       <div className="px-4 lg:px-8">
@@ -74,7 +74,7 @@ const MusicPage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="Piano solo"
+                        placeholder="Clown fish swimming around a coral reef"
                         {...field}
                       />
                     </FormControl>
@@ -99,12 +99,15 @@ const MusicPage = () => {
             </div>
           )}
 
-          {!music && !isLoading && <Empty label="No music generated." />}
+          {!video && !isLoading && <Empty label="No video gneerated." />}
 
-          {music && (
-            <audio controls className="w-full mt-8">
-              <source src={music} />
-            </audio>
+          {video && (
+            <video
+              className="w-full aspect-video mt-8 rounded-lg border bg-black"
+              controls
+            >
+              <source src={video} />
+            </video>
           )}
         </div>
       </div>
@@ -112,4 +115,4 @@ const MusicPage = () => {
   );
 };
 
-export default MusicPage;
+export default VideoPage;
--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -24,14 +24,22 @@ export async function POST(req: Request) {
     }
 
     const response = await replicate.run(
-      'riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05',
+      'anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351',
       {
         input: {
-          // alpha: 0.5,
-          prompt_a: 'prompt',
-          // prompt_b: "90's rap",
-          // denoising: 0.75,
-          // seed_image_id: 'vibes',
+          // fps: 24,
+          // model: 'xl',
+          // width: 1024,
+          // height: 576,
+          prompt,
+          //   'Clown fish swimming in a coral reef, beautiful, 8k, perfect, award winning, national geographic',
+          // batch_size: 1,
+          // num_frames: 24,
+          // init_weight: 0.5,
+          // guidance_scale: 17.5,
+          // negative_prompt:
+          //   'very blue, dust, noisy, washed out, ugly, distorted, broken',
+          // remove_watermark: false,
           // num_inference_steps: 50,
         },
       }
@@ -39,7 +47,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json(response);
   } catch (error) {
-    console.log('[MUSIC_ERROR]', error);
+    console.log('[VIDEO_ERROR]', error);
     return new NextResponse('Internal error', { status: 500 });
   }
 }
